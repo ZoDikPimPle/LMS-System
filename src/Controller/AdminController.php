e786b4780898175155e79efc02b35a072fbe42ce
Никита Филиппов <zodiak2431@gmail.com>
2024-12-22 00:07:00 +0300

    Просмотр/удаление/редактирование данных в accountsTable

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -16,21 +18,80 @@ class AdminController extends AbstractController
         ]);
     }
 
-    #[Route('/admin/disciplines', name: 'admin_disciplines')] #дисциплины
+    #[Route('/admin/disciplines', name: 'admin_disciplines')]
     public function disciplines(): Response
     {
         return $this->render('admin/disciplines.html.twig', [
             'controller_name' => 'AdminController',
         ]);
     }
-
-    #[Route('/admin/accounts', name: 'admin_accounts')] #аккаунты
+/*
+    #[Route('/admin/accounts', name: 'admin_accounts')]
     public function accounts(): Response
     {
         return $this->render('admin/accounts.html.twig', [
             'controller_name' => 'AdminController',
         ]);
     }
+*/
+    #[Route('/admin/accountsTable', name: 'admin_accounts_table')]
+    public function accountsTable(): Response
+    {
+        return $this->render('admin/accountsTable.html.twig');
+    }
+
+    #[Route('/admin/users/data', name: 'admin_users_data')]
+    public function getUsersData(UserRepository $userRepository): JsonResponse
+    {
+        $users = $userRepository->findAll();
+
+        $data = array_map(function ($user) {
+            return [
+                'id' => $user->getId(),
+                'email' => $user->getEmail(),
+                'roles' => implode(', ', $user->getRoles()), // Преобразуем массив ролей в строку
+            ];
+        }, $users);
+
+        return new JsonResponse($data);
+    }
+
+
+    #[Route('/admin/users/update', name: 'admin_users_update', methods: ['POST'])]
+    public function updateUser(Request $request, UserRepository $userRepository, EntityManagerInterface $em): JsonResponse
+    {
+    $data = json_decode($request->getContent(), true);
+    $id = $data['id'] ?? null;
+    $field = $data['field'] ?? null;
+    $value = $data['value'] ?? null;
+
+    if (!$id || !$field || !$value) {
+        return new JsonResponse(['error' => 'Invalid input'], Response::HTTP_BAD_REQUEST);
+    }
+
+    $user = $userRepository->find($id);
+
+    if (!$user) {
+        return new JsonResponse(['error' => 'User not found'], Response::HTTP_NOT_FOUND);
+    }
+
+    // Обновляем поле
+    switch ($field) {
+        case 'email':
+            $user->setEmail($value);
+            break;
+        case 'roles':
+            $user->setRoles(array_map('trim', explode(',', $value)));
+            break;
+        default:
+            return new JsonResponse(['error' => 'Invalid field'], Response::HTTP_BAD_REQUEST);
+    }
+
+    $em->flush();
+
+    return new JsonResponse(['success' => true]);
+
+    }
+
+    
 }
-
-
--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -33,28 +33,18 @@ class UserRepository extends ServiceEntityRepository implements PasswordUpgrader
         $this->getEntityManager()->flush();
     }
 
-    //    /**
-    //     * @return User[] Returns an array of User objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('u')
-    //            ->andWhere('u.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('u.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
-
-    //    public function findOneBySomeField($value): ?User
-    //    {
-    //        return $this->createQueryBuilder('u')
-    //            ->andWhere('u.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
+    /**
+     * Find users by a specific role.
+     *
+     * @param string $role
+     * @return User[]
+     */
+    public function findByRole(string $role): array
+    {
+        return $this->createQueryBuilder('u')
+            ->andWhere('JSON_CONTAINS(u.roles, :role) = 1')
+            ->setParameter('role', json_encode($role))
+            ->getQuery()
+            ->getResult();
+    }
 }
